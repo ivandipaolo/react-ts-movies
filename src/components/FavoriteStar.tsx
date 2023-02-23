e8c4e4505542553bb3ab1e48da5827207e4a0895
Ivan Di Paolo <65032224+ivandipaolo@users.noreply.github.com>
2023-02-23 22:05:49 +0100

    feat: added favorite section, reducer, star and sincronized with sessionStorage (#12)
    Co-authored-by: Ivan Di Paolo <ivan.dipaolo@meteologica.com>

---
+++ src/components/FavoriteStar.tsx
@@ -0,0 +1,36 @@
+import { useAppSelector, useAppDispatch } from '@/redux/hooks';
+import { toggleFavoriteMovie } from '@/redux/slices/favoriteMoviesSlice';
+import { useEffect, useState } from 'react';
+import React from 'react';
+
+interface Props {
+  movieId: number;
+  movieName: string;
+}
+
+export const FavoriteStar= ({ movieId, movieName }: Props) => {
+  const favoriteMovies = useAppSelector((state) => state.favoriteMovies.value);
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setIsFavorite(favoriteMovies.some((favMovie) => favMovie.id === movieId))
+  }, [favoriteMovies, movieId]);
+  
+  const handleAddToFavorites = (event: React.MouseEvent) => {
+    event.preventDefault()
+    event.stopPropagation()
+    if (movieId && movieName) {
+      setIsFavorite(!isFavorite);
+      dispatch(toggleFavoriteMovie({ id: movieId, name: movieName }));
+    }
+  };
+
+  return (
+    <button onClick={(e) => handleAddToFavorites(e)} className={isFavorite ? 'text-blue-600' : 'text-white'}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+      </svg>
+    </button>
+  );
+};
