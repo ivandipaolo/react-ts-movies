b2b729c56c69ed2c785f374f6168637da25d681f
Ivan Di Paolo <ivan.dipaolo@meteologica.com>
2023-03-26 20:20:54 +0200

    fix: assigned functionality for the unsed variables
    Task #000006

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from 'react'
-import { useQuery } from '@apollo/client';
+import { useEffect, useRef } from 'react';
 
+import { useQuery } from '@apollo/client';
 import { addMovies } from '@/redux/slices/availableMoviesSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { GetAvailableMoviesDocument, Movie } from '@/graphql/queries';
@@ -9,27 +9,31 @@ import { SearchSection } from '@/components/SearchSection';
 import { CategorySection } from '@/components/CategorySection';
 import { LatestReleasesSection } from '@/components/LatestReleasesSection';
 
-
 export const Home = () => {
-  const searchedMovies = useAppSelector((state) => state.searchedMovies.value)
-  const favoriteMovies = useAppSelector(state => state.favoriteMovies.value);
-  const { loading, error, data } = useQuery(GetAvailableMoviesDocument);
-  const dispatch = useAppDispatch()
+  const searchedMovies = useAppSelector((state) => state.searchedMovies.value);
+  const favoriteMovies = useAppSelector((state) => state.favoriteMovies.value);
+  const dispatch = useAppDispatch();
   const availableMoviesIds = useRef<number[]>([]);
+  const { loading, error, data } = useQuery(GetAvailableMoviesDocument);
+
   useEffect(() => {
-    if (data) {
+    if (!loading && !error && data) {
       availableMoviesIds.current = (data.nowPlayingMovies.concat(data.popularMovies)).map((movie: Movie) => movie.id);
       dispatch(addMovies(availableMoviesIds.current));
     }
-  }, [dispatch, data]);
+  }, [loading, error, data, dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
-    {
-      (searchedMovies.movies.length > 0 || (searchedMovies.noResults && favoriteMovies[0]))
-      ? <SearchSection />
-      : <LatestReleasesSection />
-    }
+      {searchedMovies.movies.length > 0 || (searchedMovies.noResults && favoriteMovies[0]) ? (
+        <SearchSection />
+      ) : (
+        <LatestReleasesSection />
+      )}
       <CategorySection />
-  </div>
-  )
-}
+    </div>
+  );
+};
